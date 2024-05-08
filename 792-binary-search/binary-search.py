f1040591c6f416902267ebb76366d41f46280a8e
TrinadhBotta <70365780+TrinadhBotta@users.noreply.github.com>
2024-05-08 14:17:43 +0900

    Time: 190 ms (64.94%) | Memory: 17.9 MB (96.07%) - LeetSync

--- 792-binary-search/binary-search.py
+++ 792-binary-search/binary-search.py
@@ -1,13 +1,21 @@
-class Solution:
-    def search(self, nums: List[int], target: int) -> int:
-        h = len(nums)-1
-        l = 0
-        while h>=l:
-            m = (l+h)//2
-            if nums[m] == target:
-                return(m)
-            if nums[m]>target:
-                h=m-1
-            else:
-                l=m+1
-        return(-1)
+class Solution(object):
+    def binsearch(self,arr,val,l,h):
+        m = (l+h)//2
+        if l>h:
+            return(-1)
+        if arr[m] == val:
+            return(m)
+        
+        if arr[m]<val:
+            return(self.binsearch(arr,val,m+1,h))
+        else:
+            return(self.binsearch(arr,val,l,m-1))
+    
+    
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        return(self.binsearch(nums,target,0,len(nums)-1))
