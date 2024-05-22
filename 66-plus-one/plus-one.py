9891bf287cb5f71339a6d748e94372784be376f4
TrinadhBotta <70365780+TrinadhBotta@users.noreply.github.com>
2024-05-22 21:32:17 +0900

    Time: 18 ms (38.02%) | Memory: 11.8 MB (12.36%) - LeetSync

--- 66-plus-one/plus-one.py
+++ 66-plus-one/plus-one.py
@@ -4,10 +4,8 @@ class Solution(object):
         :type digits: List[int]
         :rtype: List[int]
         """
-        carry = False
         for i in range(len(digits)-1,-1,-1):
             if digits[i]==9:
-                carry = True
                 digits[i]=0
             else:
                 digits[i]+=1
