7bb95647d5b432a750794bff4b300ebb8b021767
TrinadhBotta <70365780+TrinadhBotta@users.noreply.github.com>
2024-05-24 22:47:58 +0900

    Time: 0 ms (100.00%) | Memory: 11.5 MB (94.53%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,15 +1,16 @@
-class Solution:
-    def climbStairs(self, n: int) -> int:
-        if n<2:
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n==1 or n==2:
             return(n)
-        if n==2:
-            return(2)
-        a = 1
-        b = 2
+        a=1
+        b=2
         while n>2:
-            temp = b
-            b = a+b
-            a = temp
             n-=1
+            t=b
+            b=b+a
+            a=t
         return(b)
-        
