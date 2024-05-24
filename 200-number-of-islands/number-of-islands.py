45ae16211bd33f8e184d2036efd69f9db83b73b8
TrinadhBotta <70365780+TrinadhBotta@users.noreply.github.com>
2024-05-24 14:37:18 +0900

    Time: 249 ms (30.68%) | Memory: 30.4 MB (29.38%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,28 +1,36 @@
-class Solution:
-    def numIslands(self, grid: List[List[str]]) -> int:
-        stack = []
-        for i in range(len(grid)):
-            for j in range(len(grid[i])):
-                if grid[i][j]=="1":
-                    stack.append((i,j))
-        if len(stack)==0:
-            return(0)
-        ans = 0
+class Solution(object):
+    def numIslands(self, grid):
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+        """
+        ones = []
         seen = set()
-        d = [(0,1),(0,-1),(1,0),(-1,0)]
-        while stack:
-            ci,cj = stack.pop()
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j]=="1":
+                    ones.append((i,j))
+        
+        if len(ones)==0:
+            return(0)
+        
+        di = [[0,1],[0,-1],[1,0],[-1,0]]
+        m,n = len(grid), len(grid[0])
+        ans=0
+
+        while len(ones)>0:
+            ci,cj = ones.pop()
             if (ci,cj) in seen:
                 continue
-            queue = [(ci,cj)]
             ans+=1
-            while queue:
-                ci,cj = queue.pop()
-                seen.add((ci,cj))
-                for (i,j) in d:
-                    x = ci+i
-                    y = cj+j
-                    if -1<x<len(grid) and -1<y<len(grid[0]):
-                        if grid[x][y]=="1" and (x,y) not in seen:
-                            queue.append((x,y))
+            stack = [(ci,cj)]
+            seen.add((ci,cj))
+            while stack:
+                ci, cj = stack.pop()
+                for i, j in di:
+                    ti,tj = ci+i, cj+j
+                    if -1<ti<m and -1<tj<n and (ti,tj) not in seen and grid[ti][tj]=="1":
+                        stack.append((ti,tj))
+                        seen.add((ti,tj))
+        
         return(ans)
