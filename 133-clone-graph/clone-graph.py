754d2f423cf644d57606e4930485cf9d884f13fe
TrinadhBotta <70365780+TrinadhBotta@users.noreply.github.com>
2024-05-29 16:54:09 +0900

    Time: 31 ms (57.27%) | Memory: 11.8 MB (79.51%) - LeetSync

--- 133-clone-graph/clone-graph.py
+++ 133-clone-graph/clone-graph.py
@@ -1,37 +1,49 @@
-from collections import deque
 """
 # Definition for a Node.
-class Node:
+class Node(object):
     def __init__(self, val = 0, neighbors = None):
         self.val = val
         self.neighbors = neighbors if neighbors is not None else []
 """
 
-from typing import Optional
-class Solution:
-    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+class Solution(object):
+    def cloneGraph(self, node):
+        """
+        :type node: Node
+        :rtype: Node
+        """
         if not node:
-            return
-        queue = deque([])
-        queue.append(node)
-        clone = Node(node.val)
+            return(None)
+
+        head = Node(node.val)
+        t = head
         d = {}
-        d[1] = clone
+        d[node.val] = t
+
         seen = set()
-        while queue:
-            main_node = queue.popleft()
-            clone_node = d[main_node.val]
-            if len(main_node.neighbors)>0:
-                for neighbor_node in main_node.neighbors:
-                    if (main_node.val,neighbor_node.val) not in seen:
-                        if neighbor_node.val not in d:
-                            new_node = Node(neighbor_node.val)
-                            d[neighbor_node.val] = new_node
-                        else:
-                            new_node = d[neighbor_node.val]
-                        clone_node.neighbors.append(new_node)
-                        new_node.neighbors.append(clone_node)
-                        queue.append(neighbor_node)
-                        seen.add((main_node.val,neighbor_node.val))
-                        seen.add((neighbor_node.val,main_node.val))
-        return(clone)
+        stack = [node]
+
+        while len(stack)>0:
+            cur_node = stack.pop()
+
+            for neighbor in cur_node.neighbors:
+
+                if (cur_node.val,neighbor.val) in seen or (neighbor.val, cur_node.val) in seen:
+                    continue
+                
+                if neighbor.val not in d:
+                    new_neighbor = Node(neighbor.val)
+                    d[neighbor.val] = new_neighbor
+
+                d[cur_node.val].neighbors.append(d[neighbor.val])
+                d[neighbor.val].neighbors.append(d[cur_node.val])
+                stack.append(neighbor)
+    
+                seen.add((cur_node.val,neighbor.val))
+        
+        return(head)
+
+
+
+
+        
