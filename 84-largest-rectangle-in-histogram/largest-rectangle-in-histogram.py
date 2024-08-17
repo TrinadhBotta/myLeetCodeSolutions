d4a2e503303532805a03811279e1100bca5ff088
TrinadhBotta <70365780+TrinadhBotta@users.noreply.github.com>
2024-08-17 17:20:28 +0900

    Time: 660 ms (69.92%) | Memory: 33.7 MB (27.66%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
@@ -4,12 +4,12 @@ class Solution:
         ans = max(heights)
 
         for i in range(1,len(heights)):
-            ci = i
+            start_idx = i
             while stack and stack[-1][1]>heights[i]:
                 start_idx, hi = stack.pop()
                 ans = max(ans, hi*(i-start_idx))
-                ci = start_idx
-            stack.append((ci,heights[i]))
+    
+            stack.append((start_idx,heights[i]))
         
         for i in range(len(stack)):
             ans = max(ans, (len(heights)-stack[i][0])*stack[i][1])
