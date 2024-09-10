a1ed8ad5895131037e5333b22dd0bda337064077
TrinadhBotta <70365780+TrinadhBotta@users.noreply.github.com>
2024-09-10 15:56:00 +0900

    Time: 1558 ms (40.98%) | Memory: 59.4 MB (39.51%) - LeetSync

---
+++ 2026-merge-triplets-to-form-target-triplet/merge-triplets-to-form-target-triplet.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def mergeTriplets(self, triplets, target):
+        """
+        :type triplets: List[List[int]]
+        :type target: List[int]
+        :rtype: bool
+        """
+        a, b, c = False, False, False
+        l = []
+
+        for i in triplets:
+            if i[0]>target[0] or i[1]>target[1] or i[2]>target[2]:
+                continue
+            
+            if i[0] == target[0]:
+                a = True
+            if i[1] == target[1]:
+                b = True
+            if i[2] == target[2]:
+                c = True
+            
+        return(a and b and c)
