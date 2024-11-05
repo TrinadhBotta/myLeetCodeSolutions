4177ca4d67c8ab1015e2972c591d50733c834313
TrinadhBotta <70365780+TrinadhBotta@users.noreply.github.com>
2024-11-05 18:07:45 +0900

    Time: 0 ms (100.00%) | Memory: 11.9 MB (24.18%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.py
+++ 22-generate-parentheses/generate-parentheses.py
@@ -1,16 +1,23 @@
-class Solution:
-    def rec(self, s, o, c):
-        if o==0 and c==0:
-            self.ans.append(s)
+class Solution(object):
+    def generateParenthesis(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        ans = []
+
+        def getParenthesis(cur, o, c):
+            if o==0 and c==0:
+                ans.append(cur)
+                return
+            
+            if o>0:
+                getParenthesis(cur+"(",o-1,c)
+            
+            if c>o:
+                getParenthesis(cur+")",o,c-1)
+
             return
-        if o>=1:
-            self.rec(s+"(",o-1,c)
-        if c>o:
-            self.rec(s+")",o,c-1)
-        return
 
-
-    def generateParenthesis(self, n: int) -> List[str]:
-        self.ans = []
-        self.rec('',n,n)
-        return(self.ans)
+        getParenthesis("",n,n)
+        return(ans)
