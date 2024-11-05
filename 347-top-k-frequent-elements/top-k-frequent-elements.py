54a70a085c92d3412cac6b84f25d92fe90b7d661
TrinadhBotta <70365780+TrinadhBotta@users.noreply.github.com>
2024-11-05 14:59:34 +0900

    Time: 15 ms (11.93%) | Memory: 23.3 MB (5.71%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,20 +1,17 @@
-class Solution(object):
-    def topKFrequent(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: List[int]
-        """
+from collections import Counter
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         d = Counter(nums)
-        count = [[] for i in range(len(nums)+1)]
-        for i in d:
-            count[d[i]].append(i)
         ans=[]
-        for i in range(len(count)-1,0,-1):
-            for j in range(len(count[i])):
-                ans.append(count[i][j])
+        l = [[] for i in nums]
+        
+        for i in d:
+            l[d[i]-1].append(i)
+        
+        for i in range(len(l)-1,-1,-1):
+            for j in range(len(l[i])):
                 k-=1
-                if k == 0:
+                ans.append(l[i][j])
+                if k==0:
                     return(ans)
-        return(ans)
         
