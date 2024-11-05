733519226cdd2eb91c31923f76ff64987c7493dd
TrinadhBotta <70365780+TrinadhBotta@users.noreply.github.com>
2024-11-05 16:33:24 +0900

    Time: 403 ms (96.58%) | Memory: 19.8 MB (78.19%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,27 +1,41 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        ans=set()
-        nums.sort()
-        n=len(nums)
-        i=0
-        not_first_check = False
-        while i<n:
-            if not_first_check and nums[i]==nums[i-1]:
-                i+=1
-                continue
-            not_first_check = True
-            j=i+1
-            k=n-1
-            if nums[i]>0:
-                break
-            while j<k:
-                if nums[i]+nums[j]+nums[k]==0:
-                    ans.add((nums[i],nums[j],nums[k]))
-                    j+=1
-                    k-=1
-                elif nums[i]+nums[j]+nums[k]>0:
-                    k-=1
-                else:
-                    j+=1
-            i+=1
-        return(ans)
+        ans, pos, neg = set(), [], []
+        z = 0
+        for i in range(len(nums)):
+            if nums[i]==0:
+                z+=1
+            elif nums[i]>0:
+                pos.append(nums[i])
+            else:
+                neg.append(nums[i])
+        pos.sort()
+        neg.sort()
+        
+        if z>2:
+            ans.add((0,0,0))
+        
+        pos_set = set(pos)
+        neg_set = set(neg)
+
+        if z>0:
+
+            for i in range(len(pos)):
+                if -pos[i] in neg_set:
+                    ans.add((0,pos[i],-pos[i]))
+            
+            
+
+        for i in range(len(pos)):    
+            for j in range(i+1,len(pos)):
+                if -(pos[i]+pos[j]) in neg_set:
+                    ans.add((pos[i],pos[j],-(pos[i]+pos[j])))  
+
+        for i in range(len(neg)):    
+            for j in range(i+1,len(neg)):
+                if -(neg[i]+neg[j]) in pos_set:
+                    ans.add((neg[i],neg[j],-(neg[i]+neg[j])))
+        
+        ans = list(ans)
+        ans.sort()
+        return(ans)    
