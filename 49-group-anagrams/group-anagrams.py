a09c4c81842b9912506007e499c509ed3bd1b135
TrinadhBotta <70365780+TrinadhBotta@users.noreply.github.com>
2025-03-15 15:25:03 +0900

    Time: 47 ms (15.00%) | Memory: 16.5 MB (48.30%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,13 +1,17 @@
-class Solution:
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        ans=[]
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        strs2 = [''.join(sorted(i)) for i in strs]
+        print(strs2)
         d={}
-        ind = 0
+        ans=[]
         for i in range(len(strs)):
-            s= ''.join(sorted(strs[i]))
-            if s not in d:
-                d[s]=ind
-                ind+=1
-                ans.append([])
-            ans[d[s]]+=[strs[i]]
+            if strs2[i] in d:
+                ans[d[strs2[i]]].append(strs[i])
+            else:
+                ans.append([strs[i]])
+                d[strs2[i]]=len(ans)-1
         return(ans)
