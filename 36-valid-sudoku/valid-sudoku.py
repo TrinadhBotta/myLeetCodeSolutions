f6a5789a09f1608fb9724434ac3e2bebe4d005c9
TrinadhBotta <70365780+TrinadhBotta@users.noreply.github.com>
2025-03-15 17:42:55 +0900

    Time: 3 ms (92.26%) | Memory: 12.4 MB (55.54%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -1,14 +1,28 @@
-class Solution:
-    def isValidSudoku(self, board: List[List[str]]) -> bool:
-        res = set()
+class Solution(object):
+    def isValidSudoku(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: bool
+        """
+        r = {}
+        c = {}
+        rc = {}
+
+        for i in range(9):
+            r[i] = set()
+            c[i] = set()
+        
         for i in range(9):
             for j in range(9):
-                element = board[i][j]
-                if element != '.':
-                    if (i, element) in res or (element, j) in res or (i // 3, j // 3, element) in res :
-                        return False
-                    res.add((i, element))
-                    res.add((element, j))
-                    res.add((i // 3, j // 3, element))
+                val = board[i][j]
+                if val == ".":
+                    continue
+                if (i//3,j//3) not in rc:
+                    rc[(i//3,j//3)] = set()
+                if val in r[i] or val in c[j] or val in rc[(i//3,j//3)]:
+                    return(False)
+                r[i].add(val)
+                c[j].add(val)
+                rc[(i//3,j//3)].add(val)
 
-        return True
+        return(True)
