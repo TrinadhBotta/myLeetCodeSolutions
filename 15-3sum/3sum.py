f99cc0b341718b8daf12016ec61c68fa355e6b95
TrinadhBotta <70365780+TrinadhBotta@users.noreply.github.com>
2025-03-15 19:30:57 +0900

    Time: 893 ms (35.19%) | Memory: 18.3 MB (83.69%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,41 +1,25 @@
-class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
-        ans, pos, neg = set(), [], []
-        z = 0
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        ans=set()
+        nums.sort()
+        st = None
         for i in range(len(nums)):
-            if nums[i]==0:
-                z+=1
-            elif nums[i]>0:
-                pos.append(nums[i])
-            else:
-                neg.append(nums[i])
-        pos.sort()
-        neg.sort()
-        
-        if z>2:
-            ans.add((0,0,0))
-        
-        pos_set = set(pos)
-        neg_set = set(neg)
-
-        if z>0:
-
-            for i in range(len(pos)):
-                if -pos[i] in neg_set:
-                    ans.add((0,pos[i],-pos[i]))
-            
-            
-
-        for i in range(len(pos)):    
-            for j in range(i+1,len(pos)):
-                if -(pos[i]+pos[j]) in neg_set:
-                    ans.add((pos[i],pos[j],-(pos[i]+pos[j])))  
-
-        for i in range(len(neg)):    
-            for j in range(i+1,len(neg)):
-                if -(neg[i]+neg[j]) in pos_set:
-                    ans.add((neg[i],neg[j],-(neg[i]+neg[j])))
-        
-        ans = list(ans)
-        ans.sort()
-        return(ans)    
+            if st == nums[i]:
+                continue
+            st = nums[i]
+            j = i+1
+            k = len(nums)-1
+            tar = nums[i]
+            while j<k:
+                if tar+nums[j]+nums[k]==0:
+                    ans.add((nums[i],nums[j],nums[k]))
+                    j+=1
+                elif tar+nums[j]+nums[k]>0:
+                    k-=1
+                else:
+                    j+=1
+        return(list(ans))                    
