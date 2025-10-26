3d7c0ffd15b305bfe4c4282ffe5b7e0024642ec9
TrinadhBotta <70365780+TrinadhBotta@users.noreply.github.com>
2025-10-26 20:17:47 +0900

    Time: 2 ms (51.55%) | Memory: 23.4 MB (11.74%) - LeetSync

--- 143-reorder-list/reorder-list.py
+++ 143-reorder-list/reorder-list.py
@@ -4,46 +4,48 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def get_reverse(self, root):
-        curr = root
-        prev = None
-        while curr:
-            t = curr.next
-            curr.next = prev
-            prev = curr
-            curr = t
-        return(prev)
-
     def reorderList(self, head: Optional[ListNode]) -> None:
         """
         Do not return anything, modify head in-place instead.
         """
-        #find length
-        l = 0
+        
+        def reverseList(head: Optional[ListNode]) -> Optional[ListNode]:
+        
+            curr = head
+            prev = None
+            
+            while curr:
+                nex = curr.next
+                curr.next = prev
+                prev = curr
+                curr = nex
+            
+            return(prev)
+        
+        if not head or not head.next:
+            return(head)
+        
+        slow = head
+        fast = head
+
+        while slow and slow.next and fast and fast.next and fast.next.next:
+            slow = slow.next
+            fast = fast.next.next
+        
+        
+        to_reverse = slow.next
+        slow.next = None
+        
+        reversed_ = reverseList(to_reverse)
+        
         curr = head
-        while curr:
-            l+=1
-            curr=curr.next
-        mid = l//2 if l%2==0 else (l//2)+1
-
-        curr = head
-        while mid>0:
-            curr=curr.next
-            mid-=1
-        
-        rev_head = self.get_reverse(curr)
-        normal_head = head
-
-
-        
-        while rev_head:
-            t = normal_head.next
-            normal_head.next = ListNode(rev_head.val)
-            rev_head = rev_head.next
-            normal_head = normal_head.next
-            normal_head.next = t
-            normal_head = normal_head.next
-        
-        while head and head.next and head.next.next:
+        head = head.next
+        while head and reversed_:
+            curr.next = reversed_
+            reversed_ = reversed_.next
+            curr = curr.next
+            curr.next = head
             head = head.next
-        head.next = None
+            curr = curr.next
+        
+        curr.next = reversed_
