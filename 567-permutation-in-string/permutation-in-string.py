d08f9fd697077ca09c7486f4b21278f003ad5524
TrinadhBotta <70365780+TrinadhBotta@users.noreply.github.com>
2025-10-26 17:32:56 +0900

    Time: 23 ms (53.14%) | Memory: 18 MB (19.39%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,24 +1,26 @@
+from collections import Counter
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         if len(s1)>len(s2):
-            print('hoiio')
             return(False)
-        d = {}
-        e = {}
-        for i in range(len(s1)):
-            d[s1[i]]=d.get(s1[i],0)+1
-            e[s2[i]]=e.get(s2[i],0)+1
-        
-        if d==e:
-            return(True)
-        
-        for i in range(len(s1),len(s2)):
-            e[s2[i]]=e.get(s2[i],0)+1
-            e[s2[i-len(s1)]]=e.get(s2[i-len(s1)],0)-1
-            if e[s2[i-len(s1)]]==0:
-                del e[s2[i-len(s1)]]
-            if d==e:
-                return(True)
-        return(False)
-        
+        d1 = Counter(s1)
 
+        d2 = {}
+        for i in range(len(s1)):
+            d2[s2[i]] = d2.get(s2[i], 0)+1
+        
+        if d1==d2:
+            return(True)
+
+        print(d1,d2)
+        for i in range(len(s1), len(s2)):
+            d2[s2[i]] = d2.get(s2[i],0)+1
+            d2[s2[i-len(s1)]] = d2.get(s2[i-len(s1)],0)-1
+            if d2[s2[i-len(s1)]]==0:
+                del d2[s2[i-len(s1)]]
+
+
+            if d1==d2:
+                return(True)
+        
+        return(False)
